aee93482996648ca9201272120e17b7f5e48be54
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-03-25 23:41:34 +0200

    Initial Commit

---
+++ CountryList-SwiftUI/CountryList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryList_SwiftUIApp.swift
+//  CountryList-SwiftUI
+//
+//  Created by Noor El-Din Walid on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CountryList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
